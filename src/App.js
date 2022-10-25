fdb9303eb8f68d3cce421fc1eedcea2cae9a9b27
renanpmiranda <renanpmiranda5@gmail.com>
2022-10-25 11:31:54 -0300

    a038_exercicios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styled, { createGlobalStyle } from "styled-components";
 import TelaLogin from "./components/TelaLogin/TelaLogin";
 import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
+import React, { useState } from "react";
+import TelaUsuarioCadastrado from "./components/TelaUsuarioCadastrado/TelaUsuarioCadastrado.js";
 
 const GlobalStyled = createGlobalStyle`
   *{
@@ -14,14 +16,48 @@ const MainContainer = styled.main`
 `
 
 function App() {
+  const [tela, setTela] = useState(1)
 
-  return (
-    <MainContainer >
-      <GlobalStyled />
-      <TelaLogin />
-      <TelaCadastro />
-    </MainContainer>
-  );
+  const mudarTela = (n) => setTela(n)
+
+  switch(tela){
+    case 1:
+    return (
+        <MainContainer >
+          <GlobalStyled />
+          <TelaLogin mudarTela={mudarTela}/>
+        </MainContainer>
+      );
+    case 2:
+    return (
+        <MainContainer >
+          <GlobalStyled />
+          <TelaCadastro mudarTela={mudarTela}/>
+        </MainContainer>
+      );
+    case 3:
+      return (
+        <MainContainer >
+          <GlobalStyled />
+          <TelaUsuarioCadastrado />
+      </MainContainer>
+      )
+    default:
+      return (
+        <MainContainer >
+          <GlobalStyled />
+          <TelaLogin />
+        </MainContainer>
+      );
+  }
+
+  // return (
+  //   <MainContainer >
+  //     <GlobalStyled />
+  //     {tela === 1 ? <TelaLogin /> : <TelaCadastro />}    
+  //     {(tela && <TelaLogin />) || <TelaCadastro />}  
+  //   </MainContainer>
+  // )
 }
 
 export default App;
